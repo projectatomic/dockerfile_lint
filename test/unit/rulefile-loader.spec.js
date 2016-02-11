ad06144b9cb5640e2e72aef8377240930e9c2e09
lphiri <lphiri@redhat.com>
2016-02-11 09:08:18 -0500

    Convert linter into 'class' and rename for clarity

--- test/unit/rulefile-loader.spec.js
+++ test/unit/rulefile-loader.spec.js
@@ -2,6 +2,48 @@
 
 require('should');
 
+var loadRule = require('../../lib/rulefile-loader');
 
-// it('Should allow underscores', function() {;
-// });
+
+describe('rule file loader function', function () {
+
+    it('should throw an error when and incorrect rule specification is provided', function () {
+
+    });
+
+    it('should throw an error when when there is cyclic dependency', function () {
+
+    });
+
+    it('should throw an error when a non-existent rule file is provided', function () {
+
+
+    });
+
+    it('should return the profile name of the main file when includes are used', function () {
+
+
+    });
+
+    it('should throw an error when an included file is not found', function () {
+
+
+    });
+
+    it('should correctly parse a rule file with multiple includes', function () {
+
+    });
+
+    it('should correctly parse a rule file with an include chain', function () {
+         //
+         // We need to verify this : file B includes file A, file C includes file B, then loading
+         // file C should load rules in the order A -> B -> C, with B overriding in values in A
+         // and C overriding values in both A and B
+         //
+        var rules = loadRule('../data/rules/loader_test_include_c.yaml');
+        console.log("RULE OBJECT is : \n" + JSON.stringify(rules, null, 2));
+
+    });
+
+
+});
