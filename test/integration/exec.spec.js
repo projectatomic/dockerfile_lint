793846d22cb10ceaa2504b9b4b826da08b4ef972
Lucas Cimon <lucas.cimon@gmail.com>
2018-03-16 23:42:23 +0100

    Fixing command return code that ignored error count - fix #103

--- test/integration/exec.spec.js
+++ test/integration/exec.spec.js
@@ -7,75 +7,90 @@ var exec = require('child_process').exec,
     path = require('path')
 var binScript = path.join('bin', 'dockerfile_lint')
 
-it('should allow a valid Dockerfile', function (done) {
 
-    exec('node ' + binScript + ' -f ./test/data/dockerfiles/TestLabels',
-        function (err, stdout, stderr) {
-            if (err) {
-                return done(err);
-            }
-            stdout.trim().should.eql('# Analyzing ./test/data/dockerfiles/TestLabels\n\nCheck passed!');
-            stderr.should.eql('');
+describe('The dockerfile_lint command', function () {
+
+    it('should allow a valid Dockerfile', function (done) {
+
+        exec('node ' + binScript + ' -f ./test/data/dockerfiles/TestLabels',
+            function (err, stdout, stderr) {
+                if (err) {
+                    return done(err);
+                }
+                stdout.trim().should.eql('# Analyzing ./test/data/dockerfiles/TestLabels\n\nCheck passed!');
+                stderr.should.eql('');
+                done();
+            });
+    });
+
+    it('should validate remote (https) Dockerfile', function (done) {
+
+        exec('node ' + binScript + ' -r config/base_rules.yaml -f https://raw.githubusercontent.com/projectatomic/dockerfile_lint/master/test/data/dockerfiles/TestLabels',
+            function (err, stdout, stderr) {
+                if (err) {
+                    return done(err);
+                }
+                stdout.trim().should.eql('# Analyzing https://raw.githubusercontent.com/projectatomic/dockerfile_lint/master/test/data/dockerfiles/TestLabels\n\nCheck passed!');
+                stderr.should.eql('');
+                done();
+            });
+    });
+
+
+
+    it('should exit with a non-zero error code on an empty file', function (done) {
+        var p = exec('node ' + binScript + ' -f test/data/dockerfiles/EmptyFile -r test/data/rules/basic.yaml',
+            function (err, stdout, stderr) {
+            });
+        p.on('exit', function (code) {
+            code.should.equal(5); // 4 errors + 1 warning
             done();
         });
-});
 
-it('should validate remote (https) Dockerfile', function (done) {
+    });
 
-    exec('node ' + binScript + ' -r config/base_rules.yaml -f https://raw.githubusercontent.com/projectatomic/dockerfile_lint/master/test/data/dockerfiles/TestLabels',
-        function (err, stdout, stderr) {
-            if (err) {
-                return done(err);
-            }
-            stdout.trim().should.eql('# Analyzing https://raw.githubusercontent.com/projectatomic/dockerfile_lint/master/test/data/dockerfiles/TestLabels\n\nCheck passed!');
-            stderr.should.eql('');
+    it('should exit with code 0 on warning when in strict mode ', function (done) {
+        var p = exec('node ' + binScript + ' -p -f test/data/dockerfiles/TestLabels -r test/data/rules/basic.yaml',
+            function (err, stdout, stderr) {
+            });
+        p.on('exit', function (code) {
+            code.should.equal(0);
             done();
         });
-});
 
+    });
 
-it('should exit with code 0 on warning when in strict mode ', function (done) {
-    var p = exec('node ' + binScript + ' -p -f test/data/dockerfiles/TestLabels -r test/data/rules/basic.yaml',
-        function (err, stdout, stderr) {
+    it('should exit with code 0 on warning when in permissive mode (long form)', function (done) {
+        var p = exec('node ' + binScript + ' --permissive -f test/data/dockerfiles/TestLabels -r test/data/rules/basic.yaml',
+            function (err, stdout, stderr) {
+            });
+        p.on('exit', function (code) {
+            code.should.equal(0);
+            done();
         });
-    p.on('exit', function (code) {
-        code.should.equal(0);
-        done();
+
+    });
+
+    it('should exit with code 1 on warning when not in permissive mode ', function (done) {
+        var p = exec('node ' + binScript + ' -f test/data/dockerfiles/TestLabels -r test/data/rules/basic.yaml',
+            function (err, stdout, stderr) {
+            });
+        p.on('exit', function (code) {
+            code.should.eql(1);
+            done();
+        });
+    });
+
+
+    it('should output valid JSON when in --json mode with multiple Dockerfiles', function (done) {
+        var p = exec('node ' + binScript + ' --json -f test/data/dockerfiles/TestLabels -f test/data/dockerfiles/TestLabels -p -r test/data/rules/basic.yaml',
+            function (err, stdout, stderr) {
+                should(JSON.parse(stdout)).be.ok.and.have.lengthOf(2);
+            });
+        p.on('exit', function (code) {
+            code.should.eql(0);
+            done();
+        });
     });
 
 });
-
-it('should exit with code 0 on warning when in permissive mode (long form)', function (done) {
-    var p = exec('node ' + binScript + ' --permissive -f test/data/dockerfiles/TestLabels -r test/data/rules/basic.yaml',
-        function (err, stdout, stderr) {
-        });
-    p.on('exit', function (code) {
-        code.should.equal(0);
-        done();
-    });
-
-});
-
-it('should exit with code 1 on warning when not in permissive mode ', function (done) {
-    var p = exec('node ' + binScript + ' -f test/data/dockerfiles/TestLabels -r test/data/rules/basic.yaml',
-        function (err, stdout, stderr) {
-        });
-    p.on('exit', function (code) {
-        code.should.eql(1);
-        done();
-    });
-});
-
-
-it('should output valid JSON when in --json mode with multiple Dockerfiles', function (done) {
-    var p = exec('node ' + binScript + ' --json -f test/data/dockerfiles/TestLabels -f test/data/dockerfiles/TestLabels -p -r test/data/rules/basic.yaml',
-        function (err, stdout, stderr) {
-            should(JSON.parse(stdout)).be.ok.and.have.lengthOf(2);
-        });
-    p.on('exit', function (code) {
-        code.should.eql(0);
-        done();
-    });
-});
-
-
